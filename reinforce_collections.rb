e750857441659eebb9d26039a1590c4e84eec2b6
Billy Davidson <billy.davids52@gmail.com>
2018-08-01 15:34:11 -0400

    collections/iteration reinforcement

---
+++ reinforce_collections.rb
@@ -0,0 +1,22 @@
+room = { data: { rooms:
+    [ { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+room_201 = room[:data][:rooms][0][:capacity]
+p room_201
+
+room[:data][:events].each do |num|
+  if num[:room_id] == 1 && num[:attendees] <= 50
+    p "ok"
+  end
+    # print "find another venue"
+
+end
